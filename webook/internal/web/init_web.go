a54b59c9c5b9c0ddbc534b9bdf34b29faca71ae7
huajibin <hujiabin@thunisoft.com>
2024-09-18 21:42:56 +0800

    feat: test use middleware deal cors origin

--- webook/internal/web/init_web.go
+++ webook/internal/web/init_web.go
@@ -1,9 +1,30 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
+)
 
 func RegisterRouters() *gin.Engine {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// dev mode
+				return true
+			}
+			// your company domain
+			return strings.Contains(origin, "https://xxx.xxx.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	u := NewUserHandler()
 	u.registerUserRoutes(server)
 
