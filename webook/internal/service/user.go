b4d1ab778d43232d57255894da6f9deb1daa83bf
huajibin <hujiabin@thunisoft.com>
2024-09-22 17:20:11 +0800

    feat: Password encryption.

--- webook/internal/service/user.go
+++ webook/internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gin_test/webook/internal/domain"
 	"gin_test/webook/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -15,5 +16,11 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 }
 
 func (svc *UserService) SignUp(ctx context.Context, u domain.User) error {
+	// Password encryption
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
 	return svc.repo.Create(ctx, u)
 }
