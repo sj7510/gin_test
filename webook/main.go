c817c65b39283192f83565cbb4b19501710814bd
huajibin <hujiabin@thunisoft.com>
2024-09-24 22:29:48 +0800

    refactor: Place the initialization service in the main method to avoid circular dependencies.

--- webook/main.go
+++ webook/main.go
@@ -5,14 +5,21 @@ import (
 	"gin_test/webook/internal/repository/dao"
 	"gin_test/webook/internal/service"
 	"gin_test/webook/internal/web"
+	"gin_test/webook/internal/web/middlewire"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/contrib/cors"
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"strings"
+	"time"
 )
 
 func main() {
 	db := initDB()
 	u := initUser(db)
-	server := web.InitServer()
+	server := initServer()
 	u.RegisterUserRoutes(server)
 
 	_ = server.Run(":8080")
@@ -36,4 +43,31 @@ func initDB() *gorm.DB {
 		panic(err)
 	}
 	return db
+}
+
+func initServer() *gin.Engine {
+	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+		ExposedHeaders:   []string{"x-jwt-token"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// dev mode
+				return true
+			}
+			// your company domain
+			return strings.Contains(origin, "https://xxx.xxx.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	store := cookie.NewStore([]byte("secret"))
+	server.Use(sessions.Sessions("mysession", store))
+	server.Use(middlewire.NewLoginMiddlewareBuilder().Build())
+
+	return server
+
 }
